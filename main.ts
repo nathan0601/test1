3bca8c37786f93743e0517a019e482580f4e7a31
nathan0601 <71689307+nathan0601@users.noreply.github.com>
2020-11-24 14:21:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.West)
+})
